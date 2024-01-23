b7659d52972aa81c077a8690da0ec089f27df1cc
Joshua Banks Mailman <jmailman@alumni.uchicago.edu>
2024-01-23 00:24:25 -0500

    corrects indentation in config_template.py

--- src/config_template.py
+++ src/config_template.py
@@ -6,7 +6,7 @@ HF_API_KEY   = "<supply your hugging face api key here>"
 # This the following cycles through all active variables;
 # Whichever ones have names containing "API" will get assigned as environment vars
 def set_environment_variables():
-  variable_dict = globals().items() # all active variables including those above
-  for key, value in variable_dict:
-    if "API" in key or "ID" in key:
-      os.environ[key] = value
+    variable_dict = globals().items() # all active variables including those above
+    for key, value in variable_dict:
+        if "API" in key or "ID" in key:
+            os.environ[key] = value
